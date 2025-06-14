5eb00d8c9e62dca181c05073a881306ff591c481
Shereefalp123 <Shereefalp123@gmail.com>
2025-06-14 07:41:01 +0530

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int a = 10;
+            int b = 20;
+            Console.WriteLine(a + b);
+            Console.WriteLine(a-b);
+            Console.WriteLine(a*b);
+            Console.WriteLine(a/b);
+            Console.WriteLine(a%b);
+
+        }
+    }
+}
