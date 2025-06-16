0d4844c7f360291d1807559ae019bbf8692b2608
Shereefalp123 <Shereefalp123@gmail.com>
2025-06-16 12:09:13 +0530

    prime number

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,18 +1,21 @@
-﻿namespace ConsoleApp1
+﻿using System.Collections.Generic;
+namespace ConsoleApp1
+
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-
-            int a = 10;
-            int b = 20;
-            Console.WriteLine(a + b);
-            Console.WriteLine(a-b);
-            Console.WriteLine(a*b);
-            Console.WriteLine(a/b);
-            Console.WriteLine(a%b);
-            Console.Write(a);
+            Console.WriteLine("enter number");
+            int number =int.Parse(Console.ReadLine());
+            if (number > 0 && number % 2 !=0)
+            {
+                Console.WriteLine("number is prime");
+            }
+            else
+            {
+                Console.WriteLine("number is not prime");
+            }
 
         }
     }
