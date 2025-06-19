cdd760e0fb2cfadbf6892b5be56a93dea1fd8b6d
Shereefalp123 <Shereefalp123@gmail.com>
2025-06-19 15:45:56 +0530

    Update Program.cs
    updated

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,22 +1,36 @@
-﻿using System.Collections.Generic;
-namespace ConsoleApp1
+using System;
 
+namespace ConsoleApp1
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("enter number");
-            int number =int.Parse(Console.ReadLine());
-            if (number > 0 && number % 2 !=0)
+            Console.WriteLine("Enter number:");
+            int number = int.Parse(Console.ReadLine());
+
+            if (IsPrime(number))
             {
-                Console.WriteLine("number is prime");
+                Console.WriteLine("Number is prime");
             }
             else
             {
-                Console.WriteLine("number is not prime");
+                Console.WriteLine("Number is not prime");
+            }
+        }
+
+        static bool IsPrime(int num)
+        {
+            if (num <= 1) return false;
+            if (num == 2) return true;
+
+            for (int i = 2; i <= Math.Sqrt(num); i++)
+            {
+                if (num % i == 0)
+                    return false;
             }
 
+            return true;
         }
     }
 }
